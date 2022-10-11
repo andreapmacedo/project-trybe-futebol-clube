798770f0331c610ba32a9bbb8010497ae1cc64c4
andreapmacedo <aa.andremacedo@gmail.com>
2022-10-11 14:58:47 -0300

    implementa teste para usuário

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -1,28 +1,50 @@
 import * as chai from 'chai';
-const { expect } = require('chai');
+// const { expect } = require('chai');
 import chaiHttp = require('chai-http');
+import * as Sinon from 'sinon';
+// import app from '../src/app';
+import { app } from '../app';
+import User from '../database/models/User';
 
 
 chai.use(chaiHttp);
 
 
-// const { describe } = require('mocha');
-// const sinon = require('sinon');
+const dumpUser = {
+  name: 'Clinica X',
+  address: 'Rua X'
+}
 
-
-
-
-
-
-import { app } from '../app';
-
-// import * as sinon from 'sinon';
+// const dumpCLinicWithDoctors = {
+//   ...dumpClinic,
+//   doctors: [
+//     {
+//       id: 1,
+//       name: 'Doctor X',
+//     },
+//     {
+//       id: 2,
+//       name: 'Doctor Y',
+//     },
+//   ],
+// }
 
 describe('/users', () => {
   describe('POST', () => {
+    
+    // uso do Sinon para mockar o método create do model Clinic
+    before(() => {
+      Sinon.stub(User, 'create').resolves({  } as User)
+    });
+
+    after(() => {
+      Sinon.restore();
+    });
+    
+    
     it('should return a list of users', async () => {
-      const res = await chai.request(app).post('/users').send({ name: 'test' });
-      expect(res.status).to.equal(201);
+      const res = await chai.request(app).post('/users').send(dumpUser);
+      chai.expect(res.status).to.equal(201);
     })
   });
 }

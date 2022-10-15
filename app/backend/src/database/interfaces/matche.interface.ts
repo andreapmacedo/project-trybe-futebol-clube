79a7ad9fb5356296be2e5054744077d597dd2bde
andreapmacedo <aa.andremacedo@gmail.com>
2022-10-15 13:29:15 -0300

    requisito 22: implementa testes que cobrem 80% dos arquivos de backend.

--- app/backend/src/database/interfaces/matche.interface.ts
+++ app/backend/src/database/interfaces/matche.interface.ts
@@ -1,13 +1,23 @@
+import Matches from "../models/Matches";
+
 export interface IMatch {
   id?: number;
   homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: number;
 }
 
-// export interface IUserCreate extends IUser {
-//   id?: number;
-//   username: string;
-//   role: string;
-// }
+export interface IMatchWithTeams extends IMatch {
+  teamHome: { teamName: string };
+  teamAway: { teamName: string };
+}
+
+export interface IMatchWithTeamsModel extends Matches {
+  teamHome: { teamName: string };
+  teamAway: { teamName: string };
+}
 
 
 
